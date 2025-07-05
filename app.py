378790d33a87b8dd32e0d9ad09a6add09b48a344
Alan-1234938 <24bk1a0528@stpetershyd.com>
2025-07-05 23:17:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -85,3 +85,4 @@ app.run(host="0.0.0.0", port=port)    port = int(os.environ.get("PORT",5000))
     import os
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=port)
